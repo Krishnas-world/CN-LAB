837225650b71c276d797f626637bd0dca2b2e02d
Krishnas-world <krishnapallan128@gmail.com>
2024-12-12 21:16:34 +0530

    added 9 progs

---
+++ ContentsServer.java
@@ -0,0 +1,36 @@
+import java.net.*;
+import java.io.*;
+
+public class ContentsServer {
+  public static void main(String args[]) throws Exception {
+    // establishing the connection with the server
+    ServerSocket sersock = new ServerSocket(4000);
+    System.out.println("Server ready for connection");
+    Socket sock = sersock.accept(); // binding with port: 4000
+    System.out.println("Connection is successful and wating for chatting");
+
+    // reading the file name from client
+    InputStream istream = sock.getInputStream();
+    BufferedReader fileRead = new BufferedReader(new InputStreamReader(istream));
+    String fname = fileRead.readLine();
+    // reading file contents
+    BufferedReader contentRead = new BufferedReader(new FileReader(fname));
+
+    // keeping output stream ready to send the contents
+    OutputStream ostream = sock.getOutputStream();
+    PrintWriter pwrite = new PrintWriter(ostream, true);
+
+    String str;
+    // reading line-by-line from file
+
+    while ((str = contentRead.readLine()) != null) {
+      pwrite.println(str); // sending each line to client
+    }
+    System.out.println("Contents of the file is sent…");
+    sock.close();
+    sersock.close(); // closing network sockets
+    pwrite.close();
+    fileRead.close();
+    contentRead.close();
+  }
+}
---
+++ DSender.java
@@ -0,0 +1,16 @@
+import java.net.*;
+import java.util.*;
+
+public class DSender {
+    public static void main(String[] args) throws Exception {
+        DatagramSocket ds = new DatagramSocket();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the Message and press ENTER to Send");
+        String str = s.nextLine();
+        InetAddress ip = InetAddress.getByName("127.0.0.1");
+
+        DatagramPacket dp = new DatagramPacket(str.getBytes(), str.length(), ip, 21);
+        ds.send(dp);
+        ds.close();
+    }
+}
